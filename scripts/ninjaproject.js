20ed519d1a5f7482c8e23a4888b0a318a5e59e2d
gweniesue <all18041@byui.edu>
2020-01-18 21:22:40 -0700

    Saturday night

--- scripts/ninjaproject.js
+++ scripts/ninjaproject.js
@@ -4,21 +4,34 @@ const quiz = [
     ["What is Batman's real name?","Bruce Wayne"]
 ];
 
-let score = 0 //Initialize score
+function start(quiz){
+    let score = 0;
 
-for(const [question,answer] of quiz){
-    const response = prompt(question);
-    if(response === answer){
-        alert('Correct');
-        score ++;
+    // main game loop
+    for(const [question,answer] of quiz) {
+        const response = ask(question);
+        check(response,answer);
     }
-    else{
-        alert(`Wrong! The correct answer was ${answer}`);
+    // end of main game loop
+    gameOver();
+
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+
+    function check(response,answer){
+        if(response === answer){
+            alert('Correct!');
+            score ++;
+        }
+        else {
+            alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    function gameOver() {
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
     }
 }
-// At the end of the game, report the player's score
-alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
-
-const question = "What is Superman's real name?"
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+start(quiz);
