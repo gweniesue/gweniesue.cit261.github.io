a14ac4719892b5b41ecc97d266edbc473c0efa95
gweniesue <all18041@byui.edu>
2020-04-02 20:15:12 -0600

    thurspmup

--- week8/swapi-async.js
+++ week8/swapi-async.js
@@ -12,13 +12,16 @@ async function getJSON(url) {
         console.log(error);
     }
 }
-// model code...it is a bit redundant in this case...we could just call getJSON directly...but if our model became more complex this sets us up to accomodate that.
+// model code...it is a bit redundant in this case...we could just call getJSON directly...
+// but if our model became more complex this sets us up to accomodate that.
 function getShips(url) {
     return getJSON(url);
 }
 //  View code
 function renderShipList(ships, shipListElement) {
-    // I decided to use a table to display my list of ships. The shipList Element is that table and it has 2 children: thead and tbody...we need to put our ships into tbody...so I reference the second child.
+    // I decided to use a table to display my list of ships. The shipList Element is 
+    // that table and it has 2 children: thead and tbody...we need to put our ships into 
+    // tbody...so I reference the second child.
     const list = shipListElement.children[1];
     list.innerHTML = '';
     //loop through the ships
@@ -33,7 +36,8 @@ function renderShipList(ships, shipListElement) {
             `;
 
         listItem.addEventListener('click', function (event) {
-            //when clicked the default link behavior should be stopped, and the ship details function should be called...passing the value of the href attribute in
+            //when clicked the default link behavior should be stopped, and the ship details 
+            //function should be called...passing the value of the href attribute in
             event.preventDefault();
             getShipDetails(ship.url);
         });
@@ -58,7 +62,11 @@ async function showShips(url = 'https://swapi.co/api/starships/') {
     // enable the next and prev buttons.
     if (results.next) {
         const next = document.getElementById('next');
-        // normally we would prefer the addEventListener method of adding a listener. Using something like 'element.onEvent = event_function' has the limitation of only being able to hold one listener of the type. In this case that is a good thing however. Because we are not re-creating the buttons each time we load a new batch of data we could end up with several listeners attached to each button by the last page. We won't have that issue here.
+        // normally we would prefer the addEventListener method of adding a listener. Using something 
+        // like 'element.onEvent = event_function' has the limitation of only being able to hold one 
+        // listener of the type. In this case that is a good thing however. Because we are not re-creating 
+        // the buttons each time we load a new batch of data we could end up with several listeners attached 
+        // to each button by the last page. We won't have that issue here.
         next.ontouchend = () => {
             // notice to show the next page we just re-call the showShips function with a new URL
             showShips(data.next);
@@ -66,7 +74,9 @@ async function showShips(url = 'https://swapi.co/api/starships/') {
     }
     if (results.previous) {
         const prev = document.getElementById('prev');
-        // we need to set the once option on the listener since the buttons do not get recreated with each page load. If we don't we will end up with several listeners all trying to fire at once after a few pages
+        // we need to set the once option on the listener since the buttons do not get recreated with 
+        //each page load. If we don't we will end up with several listeners all trying to fire at once 
+        // after a few pages
         prev.ontouchend = () => {
             showShips(data.previous);
         };
--- week8/swapi-promises.js
+++ week8/swapi-promises.js
@@ -12,13 +12,16 @@ function getJSON(url) {
             console.log(error);
         });
 }
-// model code...it is a bit redundant in this case...we could just call getJSON directly...but if our model became more complex this sets us up to accomodate that.
+// model code...it is a bit redundant in this case...we could just call 
+// getJSON directly...but if our model became more complex this sets us up to accomodate that.
 function getShips(url) {
     return getJSON(url);
 }
 //  View code
 function renderShipList(ships, shipListElement) {
-    // I decided to use a table to display my list of ships. The shipList Element is that table and it has 2 children: thead and tbody...we need to put our ships into tbody...so I reference the second child.
+    // I decided to use a table to display my list of ships. The shipList 
+    // Element is that table and it has 2 children: thead and tbody...we 
+    // need to put our ships into tbody...so I reference the second child.
     const list = shipListElement.children[1];
     list.innerHTML = '';
     //loop through the ships
@@ -33,7 +36,8 @@ function renderShipList(ships, shipListElement) {
             `;
 
         listItem.addEventListener('click', function (event) {
-            //when clicked the default link behavior should be stopped, and the ship details function should be called...passing the value of the href attribute in
+            // when clicked the default link behavior should be stopped, and the ship details 
+            // function should be called...passing the value of the href attribute in
             event.preventDefault();
             getShipDetails(ship.url);
         });
@@ -60,7 +64,12 @@ function showShips(url = 'https://swapi.co/api/starships/') {
         // enable the next and prev buttons.
         if (data.next) {
             const next = document.getElementById('next');
-            // normally we would prefer the addEventListener method of adding a listener. Using something like 'element.onEvent = event_function' has the limitation of only being able to hold one listener of the type we choose. In this case that is a good thing however. Because we are not re-creating the buttons each time we load a new batch of data we could end up with several listeners attached to each button by the last page. We won't have that issue here.
+            // normally we would prefer the addEventListener method of adding a listener. 
+            // Using something like 'element.onEvent = event_function' has the limitation of 
+            // only being able to hold one listener of the type we choose. In this case that 
+            // is a good thing however. Because we are not re-creating the buttons each time we 
+            // load a new batch of data we could end up with several listeners attached to each 
+            // button by the last page. We won't have that issue here.
             next.ontouchend = () => {
                 // notice to show the next page we just re-call the showShips function with a new URL
                 showShips(data.next);
