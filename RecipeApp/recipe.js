adbb5bfe2c2a64d7e2fb4e8fa515db2f70642d8a
gweniesue <all18041@byui.edu>
2020-04-03 19:09:32 -0600

    friupdate

--- RecipeApp/recipe.js
+++ RecipeApp/recipe.js
@@ -6,7 +6,7 @@ const lambButton = document.getElementById('lamb');
 const seafoodButton = document.getElementById('seafood');
 const vegetarianButton = document.getElementById('vegetarian');
 const veganButton = document.getElementById('vegan');
-const recipe = document.getElementById('recipeDiv');
+const recipes = document.getElementById('recipeDiv');
 
 // assign URLs to variables
 
@@ -23,105 +23,73 @@ const veganURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan';
 // assigning event handler to beef
 beefButton.addEventListener('click', () => {
   fetch(beefURL)
-  .then( response => {
-    recipe.innerHTML = 'Waiting for response...';
-  if (response.ok) {
-      return response;
-  }
-      throw Error(response.statusText);
-  })
+  .then( response => {return response;})
   .then( response => response.text() )
-  .then( text => recipe.innerText = text )
+  .then( text => recipes.innerText = text )
   .catch( error => console.log('there was an error:', error))
-  console.log(beefURL);
 }, false);
 
 // assigning event handler to chicken
 chickenButton.addEventListener('click', () => {
   fetch(chickenURL)
-  .then( response => {
-    recipe.innerHTML = 'Waiting for response...';
-  if (response.ok) {
-      return response;
-  }
-      throw Error(response.statusText);
-  })
+  .then( response => {return response;})
   .then( response => response.text() )
-  .then( text => recipe.innerText = text )
+  .then( text => recipes.innerText = text )
   .catch( error => console.log('there was an error:', error))
 }, false);
 
 // assigning event handler to pork
 porkButton.addEventListener('click', () => {
   fetch(porkURL)
-  .then( response => {
-    recipe.innerHTML = 'Waiting for response...';
-  if (response.ok) {
-      return response;
-  }
-      throw Error(response.statusText);
-  })
+  .then( response => {return response;})
   .then( response => response.text() )
-  .then( text => recipe.innerText = text )
+  .then( text => recipes.innerText = text )
   .catch( error => console.log('there was an error:', error))
 }, false);
 
 // assigning event handler to lamb
 lambButton.addEventListener('click', () => {
   fetch(lambURL)
-  .then( response => {
-    recipe.innerHTML = 'Waiting for response...';
-  if (response.ok) {
-      return response;
-  }
-      throw Error(response.statusText);
-  })
+  .then( response => {return response;})
   .then( response => response.text() )
-  .then( text => recipe.innerText = text )
+  .then( text => recipes.innerText = text )
   .catch( error => console.log('there was an error:', error))
 }, false);
 
 // assigning event handler to Seafood button
 seafoodButton.addEventListener('click', () => {
   fetch(seafoodURL)
-  .then( response => {
-    recipe.innerHTML = 'Waiting for response...';
-  if (response.ok) {
-      return response;
-  }
-      throw Error(response.statusText);
-  })
+  .then( response => {return response;})
   .then( response => response.text() )
-  .then( text => recipe.innerText = text )
+  .then( text => recipes.innerText = text )
   .catch( error => console.log('there was an error:', error))
 }, false);
 
 // assigning event handler to Vegetarian
 vegetarianButton.addEventListener('click', () => {
   fetch(vegetarianURL)
-  .then( response => {
-    recipe.innerHTML = 'Waiting for response...';
-  if (response.ok) {
-      return response;
-  }
-      throw Error(response.statusText);
-  })
+  .then( response => {return response;})
   .then( response => response.text() )
-  .then( text => recipe.innerText = text )
+  .then( text => recipes.innerText = text )
   .catch( error => console.log('there was an error:', error))
 }, false);
 
 // assigning event handler to Vegan button
 veganButton.addEventListener('click', () => {
   fetch(veganURL)
-  .then( response => {
-    recipe.innerHTML = 'Waiting for response...';
-  if (response.ok) {
-      return response;
-  }
-      throw Error(response.statusText);
-  })
+  .then( response => {return response;})
   .then( response => response.text() )
-  .then( text => recipe.innerText = text )
+  .then( text => recipes.innerText = text )
   .catch( error => console.log('there was an error:', error))
 }, false);
+
+
+
+function appendRecipeList(data){
+  let mainContainer = document.getElementById("recipeDiv");
+  for (let i = 0; data.length; i++){
+    let div = document.createElement("div");
+    div.innerHTML = data[i].strMealThumb + '/preview';
+    mainContainer.appendChild(div);
+  }
+};
